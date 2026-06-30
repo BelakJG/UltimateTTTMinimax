385d2daff0517332332fe12213c07853b8c51999
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-06-30 15:50:13 -0400

    function to print out ultimate TTT board

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from functools import cache
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_board(board_string):
+    boards_array = board_string.split("/")
+
+    for outer_row in range(3):
+        for inner_row in range(3):
+            r = outer_row * 3 + inner_row
+            row = boards_array[r]
+
+            output = []
+
+            for big_col in range(3):
+                chunk = row[big_col*3:(big_col+1)*3]
+
+                for inner_col, cell in enumerate(chunk):
+                    if cell == ".":
+                        index = inner_row * 3 + inner_col
+                        output.append(str(index))
+                    else:
+                        output.append(cell)
+
+                if big_col != 2:
+                    output.append("|")
+
+            print(" ".join(output))
+
+        if outer_row != 2:
+            print("------+-------+------")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    board = "........./........./........./........./........./........./........./........./........."
+    print_board(board)
